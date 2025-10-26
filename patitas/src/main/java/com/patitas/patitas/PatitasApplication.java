e9aef753205ae21f16f93d6e8f1a0a27845afd53
santiagoflaugnacco-glitch <santiagoflaugnacco@gmail.com>
2025-10-26 19:14:06 -0300

    Add files via upload

---
+++ patitas/src/main/java/com/patitas/patitas/PatitasApplication.java
@@ -0,0 +1,13 @@
+package com.patitas.patitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasApplication.class, args);
+	}
+
+}
---
+++ patitas/src/test/java/com/patitas/patitas/PatitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patitas.patitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
